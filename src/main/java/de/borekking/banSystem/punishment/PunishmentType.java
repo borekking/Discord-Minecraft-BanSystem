51d32cbc59ec06025525d18570add3ede950ca68
borekking <mcnairolfflo@gmail.com>
2022-06-04 16:44:17 +0200

    PunishmentType: Changed happenings on (un)mute, (un)ban - disconnecting, messages, ...

--- src/main/java/de/borekking/banSystem/punishment/PunishmentType.java
+++ src/main/java/de/borekking/banSystem/punishment/PunishmentType.java
@@ -2,35 +2,61 @@ package de.borekking.banSystem.punishment;
 
 import de.borekking.banSystem.BungeeMain;
 import de.borekking.banSystem.config.ConfigSetting;
+import de.borekking.banSystem.punishment.user.User;
 import de.borekking.banSystem.util.discord.DiscordUtils;
 
+import java.util.List;
+import java.util.UUID;
 import java.util.function.Consumer;
 
 import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.User;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public enum PunishmentType {
 
     BAN(punishment -> {
+        // Ban
         Platform platform = punishment.getPlatform();
+        User user = BungeeMain.getUser(punishment.getUserID());
         switch (platform) {
             case DISCORD:
                 // Ban user from discord guild
-                User discordUser = DiscordUtils.getUser(punishment.getUserID());
-                String reason = ConfigSetting.BAN_DISCORD_MESSAGE.getValueAsString().replace("%reason%", punishment.getReason());
-                DiscordUtils.ban(discordUser, 2, reason);
+                List<Long> discordIDs = user.getDiscordIDs();
+                for (Long id : discordIDs) {
+                    net.dv8tion.jda.api.entities.User discordUser = DiscordUtils.getUser(id);
+                    if (discordUser == null) continue; // NPE!
+
+                    String reason = ConfigSetting.BAN_DISCORD_MESSAGE.getValueAsString().replace("%reason%", punishment.getReason());
+                    DiscordUtils.ban(discordUser, 2, reason); // TODO delDays
+                }
                 break;
             case MINECRAFT:
-                // No Action on minecraft ban
+                // Kick user from Server (MC)
+                List<UUID> uuids = user.getUuids();
+                String reason = ConfigSetting.BAN_MINECRAFT_MESSAGE.getValueAsString().replace("%reason%", punishment.getReason());
+                for (UUID uuid : uuids) {
+                    ProxiedPlayer player = BungeeMain.getPlayer(uuid);
+                    if (player == null) continue; // NPE!
+
+                    player.disconnect(new TextComponent(reason));
+                }
                 break;
         }
     }, punishment -> {
+        // Unban
         Platform platform = punishment.getPlatform();
+        User user = BungeeMain.getUser(punishment.getUserID());
         switch (platform) {
             case DISCORD:
-                // Unban user from discord
-                User discordUser = DiscordUtils.getUser(punishment.getUserID());
-                DiscordUtils.unban(discordUser);
+                // Unban user from discord guild
+                List<Long> discordIDs = user.getDiscordIDs();
+                for (Long id : discordIDs) {
+                    net.dv8tion.jda.api.entities.User discordUser = DiscordUtils.getUser(id);
+                    if (discordUser == null) continue; // NPE!
+
+                    DiscordUtils.unban(discordUser);
+                }
                 break;
             case MINECRAFT:
                 // No Action on minecraft unban
@@ -38,34 +64,78 @@ public enum PunishmentType {
         }
     }),
     MUTE(punishment -> {
+        // Mute
         Platform platform = punishment.getPlatform();
+        User user = BungeeMain.getUser(punishment.getUserID());
         switch (platform) {
-            case DISCORD:
-                // Add mute role
-                User discordUser = DiscordUtils.getUser(punishment.getUserID());
+            case DISCORD: {
                 Role muteRole = BungeeMain.getMuteRole();
                 if (muteRole == null) break; // TODO - MSG?
 
-                DiscordUtils.addRole(discordUser, muteRole);
+                String message = ConfigSetting.MUTE_DISCORD_MUTE.getValueAsString().replace("%reason%", punishment.getReason());
+
+                // Add mute role to user and send DM
+                List<Long> discordIDs = user.getDiscordIDs();
+                for (Long id : discordIDs) {
+                    net.dv8tion.jda.api.entities.User discordUser = DiscordUtils.getUser(id);
+                    if (discordUser == null) continue; // NPE!
+
+                    DiscordUtils.addRole(discordUser, muteRole);
+
+                    discordUser.openPrivateChannel().queue(channel -> channel.sendMessage(message).queue());
+                }
                 break;
-            case MINECRAFT:
-                // No action on minecraft mute
+            }
+            case MINECRAFT: {
+                // Send message to minecraft user
+                String message = ConfigSetting.MUTE_MINECRAFT_MUTE.getValueAsString().replace("%reason%", punishment.getReason());
+
+                List<UUID> uuids = user.getUuids();
+                for (UUID uuid : uuids) {
+                    ProxiedPlayer player = BungeeMain.getPlayer(uuid);
+                    if (player == null) continue; // NPE!
+
+                    BungeeMain.sendMessage(player, message);
+                }
                 break;
+            }
         }
     }, punishment -> {
+        // Unmute
         Platform platform = punishment.getPlatform();
+        User user = BungeeMain.getUser(punishment.getUserID());
         switch (platform) {
-            case DISCORD:
+            case DISCORD: {
                 // Remove mute role
-                User discordUser = DiscordUtils.getUser(punishment.getUserID());
                 Role muteRole = BungeeMain.getMuteRole();
                 if (muteRole == null) break; // TODO - MSG?
 
-                DiscordUtils.removeRole(discordUser, muteRole);
+                String message = ConfigSetting.MUTE_DISCORD_UNMUTE.getValueAsString().replace("%reason%", punishment.getReason());
+
+                // Add mute role to user and send DM
+                List<Long> discordIDs = user.getDiscordIDs();
+                for (Long id : discordIDs) {
+                    net.dv8tion.jda.api.entities.User discordUser = DiscordUtils.getUser(id);
+                    if (discordUser == null) continue; // NPE!
+
+                    DiscordUtils.addRole(discordUser, muteRole);
+                    discordUser.openPrivateChannel().queue(channel -> channel.sendMessage(message).queue());
+                }
                 break;
-            case MINECRAFT:
-                // No action on minecraft unmute
+            }
+            case MINECRAFT: {
+                // Send message to minecraft user
+                String message = ConfigSetting.MUTE_MINECRAFT_MUTE.getValueAsString().replace("%reason%", punishment.getReason());
+
+                List<UUID> uuids = user.getUuids();
+                for (UUID uuid : uuids) {
+                    ProxiedPlayer player = BungeeMain.getPlayer(uuid);
+                    if (player == null) continue; // NPE!
+
+                    BungeeMain.sendMessage(player, message);
+                }
                 break;
+            }
         }
     });
 
