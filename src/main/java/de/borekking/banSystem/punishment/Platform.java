f3486532814b69f14f250142155636b6d1a8e474
borekking <mcnairolfflo@gmail.com>
2022-06-13 19:05:30 +0200

    Added Some Platform methods for getting UserIDs and checking for them

--- src/main/java/de/borekking/banSystem/punishment/Platform.java
+++ src/main/java/de/borekking/banSystem/punishment/Platform.java
@@ -1,21 +1,46 @@
 package de.borekking.banSystem.punishment;
 
+import de.borekking.banSystem.util.BSUtils;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public enum Platform {
 
-    DISCORD("discordUser", "discordID", "d"),
-    MINECRAFT("minecraftUser", "minecraftID", "m");
+    DISCORD("discordUser", "discordID", "d",
+            id -> BSUtils.getDiscordID(id) >= 0,
+            BSUtils::getUserIDByDiscord,
+            BSUtils::getUserIDByDiscordIDAndCreateIfAbsent),
+    MINECRAFT("minecraftUser", "minecraftID", "m",
+            id -> BSUtils.getUUID(id) != null,
+            BSUtils::getUserIDByMinecraft,
+            BSUtils::getUserIDByMinecraftIDAndCreateIfAbsent);
 
     private final String databaseTableName, columnName, identifier;
 
-    Platform(String databaseTableName, String columnName, String identifier) {
+    private final Predicate<String> platformIDIsValid;
+
+    private final Function<String, Long> getUserIDFunction;
+
+    private final BiFunction<String, String, Long> getUserIDAndCreateIfAbsentFunction;
+
+    Platform(String databaseTableName, String columnName, String identifier,
+             Predicate<String> platformIDIsValid,
+             Function<String, Long> getUserIDFunction,
+             BiFunction<String, String, Long> getUserIDAndCreateIfAbsentFunction) {
+
         this.databaseTableName = databaseTableName;
         this.columnName = columnName;
         this.identifier = identifier;
+
+        this.platformIDIsValid = platformIDIsValid;
+        this.getUserIDFunction = getUserIDFunction;
+        this.getUserIDAndCreateIfAbsentFunction = getUserIDAndCreateIfAbsentFunction;
     }
 
     public static Platform getByIdentifier(String identifier) {
@@ -38,6 +63,22 @@ public enum Platform {
     public static long getDiscordID(String id) {
         if (id == null) return -1L;
         return Long.parseLong(id);
+    }
+
+    public boolean platformIDIsValid(String platformID) {
+        return this.platformIDIsValid.test(platformID);
+    }
+
+    public boolean userExists(String platformID) {
+        return this.getUserID(platformID) >= 0;
+    }
+
+    public long getUserID(String platformID) {
+        return this.getUserIDFunction.apply(platformID);
+    }
+
+    public long getUserIDAncCreateIfAbsent(String platformID, String permissionOnAbsent) {
+        return this.getUserIDAndCreateIfAbsentFunction.apply(platformID, permissionOnAbsent);
     }
 
     public String getDatabaseTableName() {
