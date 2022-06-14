065cd9dce98593fc1c31f55fe9cc7e8b64f55362
borekking <mcnairolfflo@gmail.com>
2022-06-14 18:16:50 +0200

    Added Function to create a random long starting at a specified value and madein RandomNumberCreator.

--- src/main/java/de/borekking/banSystem/util/RandomNumberCreator.java
+++ src/main/java/de/borekking/banSystem/util/RandomNumberCreator.java
@@ -6,12 +6,29 @@ public final class RandomNumberCreator {
 
     private static Random random;
 
+    private RandomNumberCreator() {
+    }
+
     public static long getRandomLong() {
+        updateRandom();
+
+        return random.nextLong();
+    }
+
+    public static long getRandomLong(long from) {
+        long l;
+
+        do {
+            l = getRandomLong();
+        } while (l < from);
+
+        return l;
+    }
+
+    private static void updateRandom() {
         if (random == null) {
             random = new Random();
         }
-
-        return random.nextLong();
     }
 
 }
