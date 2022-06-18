c9fb5ca9711c9979e3001f929dad0df751bad232
borekking <mcnairolfflo@gmail.com>
2022-06-18 17:53:05 +0200

    Added Methode to get a user's name in User

--- src/main/java/de/borekking/banSystem/punishment/user/User.java
+++ src/main/java/de/borekking/banSystem/punishment/user/User.java
@@ -1,6 +1,8 @@
 package de.borekking.banSystem.punishment.user;
 
 import de.borekking.banSystem.punishment.Platform;
+import de.borekking.banSystem.util.discord.DiscordUtils;
+import de.borekking.banSystem.util.minecraft.MinecraftUUIDUtils;
 import java.util.List;
 import java.util.UUID;
 
@@ -22,6 +24,18 @@ public class User {
         this.permissions = permissions;
         this.uuids = Platform.convertMinecraftIDs(uuids);
         this.discordIDs = Platform.convertDiscordIDs(discordIDs);
+    }
+
+    public String getName() {
+        if (this.uuids.size() > 0) {
+            return MinecraftUUIDUtils.getNameFromUUID(uuids.get(0));
+        }
+
+        if (this.discordIDs.size() > 0) {
+            return DiscordUtils.getUserByID(this.discordIDs.get(0)).getName();
+        }
+
+        return "";
     }
 
     public long getId() {
